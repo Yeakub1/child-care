18a796d7b65968fd268a2cbf2bbcbe1b2c8ac23c
Yeakub1 <mryeakub10@gmail.com>
2023-08-07 13:23:43 +0600

    home

--- src/components/Pages/Home/Banner/Banner.jsx
+++ src/components/Pages/Home/Banner/Banner.jsx
@@ -3,26 +3,30 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="">
-        <div
-          className="hero min-h-screen"
-          style={{
-            backgroundImage:
-              "url(https://i.ibb.co/xDRZRkD/tutor-with-litthe-girl-studying-home.jpg)",
-          }}
-        >
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-              <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/xDRZRkD/tutor-with-litthe-girl-studying-home.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="">
+            <h1 className="mb-5 text-5xl font-bold">
+              <h1>Welcome to Our</h1>
+              <h1 className="text-[#fbb44d] mt-7">Academy for Bright Children</h1>
+            </h1>
+            <p className="mb-5">
+              Fusce pretium, est at aliquam semper, leo ante facilisis risus in
+              feugiat ipsum augue pellent esque metu at luctus metus, non ornare
+              lectus ac ipsum convallis fermentum ante a suscipit nunc. Etiam
+              non scelerisque loren ornare dapibus libero sit amet facil sodales
+              rutrum .
+            </p>
           </div>
         </div>
+      </div>
     </div>
   );
 };
